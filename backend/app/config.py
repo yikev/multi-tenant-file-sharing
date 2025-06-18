c73879bbba498566ffe79ee92373b257cbb83e88
yikev <westcoastkevin@gmail.com>
2025-06-17 18:22:06 -0700

    removed secret key, setup .env

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,12 +1,14 @@
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
 import os
 
+load_dotenv() 
 
 class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     class Config:
         env_file = ".env"
