ecb1772695c1f9588eebbeed18880e7f65c4bf44
yikev <westcoastkevin@gmail.com>
2025-06-23 00:36:55 -0700

    update

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,8 @@ import { HashRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
+console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
